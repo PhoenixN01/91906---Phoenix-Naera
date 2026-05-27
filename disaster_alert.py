6e2075218a4c9762cc8134d4798422998c9a00c3
Phoenix N <pn22178@my.westlake.school.nz>
2026-05-27 14:21:14 +1200

    Begun working on populating frames. Refresh, location have been finished and main status window started

--- disaster_alert.py
+++ disaster_alert.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk
+from datetime import datetime
 
 class disasterApp:
     def __init__(self, root):
@@ -10,10 +11,27 @@ class disasterApp:
         self.root.minsize(496, 496)
         self.root.geometry("496x496+500+0")
 
+        current_time = datetime.now()
+        
+        self.last_all_refresh = \
+            current_time.strftime("%Y-%m-%d %I:%M:%S %p")
+        
         self.create_frames()
+        self.create_widgets()
+
+        self.main_frame.pack(fill="both", expand=True)
+        self.refresh_frame.pack(fill="x")
+        self.location_frame.pack(fill="x")
+        self.all_status_frame.pack(fill="x", expand=True)
     
     def create_frames(self):
+        """Creates the layout structure of the GUI\
+        
+        The usage of frames allows GUI elements to be placed without 
+        encountering issues of mixing information across the GUI
+        """
         self.main_frame = ttk.Frame(self.root)
+        self.refresh_frame = ttk.Frame(self.main_frame)
         self.location_frame = ttk.Frame(self.main_frame, padding=10)
         self.all_status_frame = ttk.Frame(self.main_frame, padding=10)
         self.footer_frame = ttk.Frame(self.main_frame, padding=10)
@@ -44,10 +62,38 @@ class disasterApp:
             ttk.Frame(self.earthquake_frame, padding=10)
         self.q_footer_frame = \
             ttk.Frame(self.earthquake_frame, padding=10)
+    
+    def create_widgets(self):
+        self.last_refresh_label = ttk.Label(
+            self.refresh_frame, 
+            text=f"Last refreshed at:    {self.last_all_refresh}",
+            padding=10
+            )
+        self.last_refresh_label.pack(side="top")
+
+        self.current_location_label = ttk.Label(
+            self.location_frame,
+            text="Current Location: ____, ______",
+            padding=(10, 0)
+        )
+        self.current_location_label.grid(row=0, column=0)
+
+        self.change_location_button = ttk.Button(
+            self.location_frame,
+            text="Change",
+        )
+        self.change_location_button.grid(row=0, column=1)
+        
+        self.search_radius_label = ttk.Label(
+            self.location_frame,
+            text="Radius: _km",
+            padding=(10, 0)
+        )
+        self.search_radius_label.grid(row=1, column=0)
+
         
         
         
-
-
-
 root = tk.Tk()
+app = disasterApp(root)
+root.mainloop()
